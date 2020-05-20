68297169ca1a931c8598fc379704c7ff526808d4
Kasthuri Mahadeva <kasthuri@unicomtechnology.co.uk>
2020-05-20 06:03:50 +0530

    basic-stop-watch

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+            Console.WriteLine("Welcome to Panda-Karady\'s Stop Watch");
+            Console.WriteLine("Press 1 - Start, 2 - Stop, 0 - Exit");
+            while (true)
+            {
+                var userInput = Console.ReadLine();
+                var isNumber = int.TryParse(userInput, out var value);
+                if (isNumber)
+                {
+                    if (value == 1)
+                    {
+                        stopWatch.Start();
+                    } else if (value == 2)
+                    {
+                        stopWatch.Stop();
+                    }
+                    else if(value == 0)
+                    {
+                        Console.WriteLine("**********************");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not valid input. Try again...");
+                    }
+                } else
+                {
+                    Console.WriteLine("Not valid input. Try again...");
+                }
+            }
+        }
+    }
+}
---
+++ StopWatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class StopWatch
+    {
+        private bool IsStarted { get; set; }
+        private DateTime StartTime { get; set; }
+        private DateTime EndTime { get; set; }
+
+        public StopWatch()
+        {
+            IsStarted = false;
+        }
+
+        public void Start()
+        {
+            if (!IsStarted)
+            {
+                Console.WriteLine("Stop watch started");
+                IsStarted = true;
+                StartTime = DateTime.Now;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stop watch is already started");
+            }
+        }
+
+        public void Stop()
+        {
+            if (IsStarted)
+            {
+                Console.WriteLine("Stop watch stopped");
+                IsStarted = false;
+                EndTime = DateTime.Now;
+                var timeSpan = EndTime - StartTime;
+                Console.WriteLine($"Duration is {timeSpan.Seconds} sec");
+            }
+            else
+            {
+                throw new InvalidOperationException("Stopwatch is not started yet");
+            }
+        }
+    }
+}
